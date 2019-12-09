b2f12f69de63475ea53f339d72f930685df9d265
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-12-09 18:05:59 +0530

    Create SpringBootMainApplication.java

---
+++ src/main/java/com/arya/demo/SpringBootMainApplication.java
@@ -0,0 +1,12 @@
+package com.arya.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMainApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootMainApplication.class, args);
+	}
+}
