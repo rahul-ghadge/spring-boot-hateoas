9856509e00cdd72754aa066630d30dc9e96bd576
Rahul Ghadge <54259667+rahul-ghadge@users.noreply.github.com>
2019-12-09 18:08:41 +0530

    Update ErrorDetails.java

--- src/main/java/com/arya/demo/exception/ErrorDetails.java
+++ src/main/java/com/arya/demo/exception/ErrorDetails.java
@@ -7,19 +7,33 @@ public class ErrorDetails {
 
 	private Date timestamp;
 	
+	private int status;
+	
+	private String error;
+	
 	private String message;
 	
 	private String details;
 
-	public ErrorDetails(Date timestamp, String message, String details) {
+	public ErrorDetails(Date timestamp, int status, String error, String message, String details) {
 		super();
 		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
 		this.message = message;
 		this.details = details;
 	}
 
 	public Date getTimestamp() {
 		return timestamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getError() {
+		return error;
 	}
 
 	public String getMessage() {
